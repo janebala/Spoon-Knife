aa846707dc0b0b32856bc1388c9cb960b40a4f15
majanebala <majanebala@gmail.com>
2017-09-24 00:11:40 +0800

    Update Program.cs

--- HelloWorld/HelloWorld/Program.cs
+++ HelloWorld/HelloWorld/Program.cs
@@ -11,7 +11,7 @@ namespace HelloWorld
         static void Main(string[] args)
         {
             //Hello world lol ---LOL
-            //
+            //LOLLLLL
             //LOLL
         }
     }
